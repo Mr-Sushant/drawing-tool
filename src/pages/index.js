443e25ac486e85fb9de23271db14451c32e27f23
Sushant Shukla <1805352@kiit.ac.in>
2023-09-29 15:21:25 +0530

    add icon and title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import Menu from "@/components/Menu";
 import Toolbox from "@/components/Toolbox";
 import Board from "@/components/Board";
+import Head from 'next/head'
+
+
 
 export default function Home() {
   return (
     <>
+    <Head>
+        <title>DrawConnect</title>
+        <link rel="icon" href="/download.svg"/>
+      </Head>
     <Menu />
     <Toolbox />
     <Board />
